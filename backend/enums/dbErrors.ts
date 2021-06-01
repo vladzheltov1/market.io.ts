b9433a3cb614bf259ad10a995c62d69c72d38e39
Vladislav Zheltov <74919692+vladzheltov1@users.noreply.github.com>
2021-06-01 10:14:13 +0300

    Delete backend directory

--- backend/enums/dbErrors.ts
+++
@@ -1,6 +0,0 @@
-export enum dbErrors{
-    ECONNREFUSED = "Нет подключения к базе данных!",
-    NOTFOUND = "По вашему запросу ничего не найдено!",
-    CANTCONNECT = "Не удалось подключиться к базе данных!",
-    EMPTYQUERY = "Пустой SQL запрос!"
-}
--- backend/modules/routerInit.mod.ts
+++
@@ -1,3 +0,0 @@
-const { Router } = require('express');
-
-export const RouterServer = Router();
