39df8f11c18b41bb0fea15cf8a64f77d5bf8b344
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2021-08-22 13:06:51 +0300

    UIkit token system init

---
+++ client/src/components/UIkit/token.js
@@ -0,0 +1,48 @@
+export const UItoken = {
+    colorRedNormal: "#eb4d42",
+    colorRedActive: "#dc4338",
+    colorOrangeNormal: "#f6781d",
+    colorOrangeActive: "#e7701b",
+    colorYellowNormal: "#ffcc00",
+    colorYellowActive: "#f5c400",
+    colorGreenNormal: "#4caf50",
+    colorGreenActive: "#46a149",
+    colorBlueNormal: "#3e75ec",
+    colorBlueActive: "#386ddd",
+    colorVioletNormal: "#7e22ce",
+    colorVioletActive: "#721fbb",
+    colorWhite: "#fff",
+    colorGray_light: "#f0f2f4",
+    colorGray_medium: "#ced4da",
+    colorGray_dark: "#7b8691",
+    colorGray_black: "#4d565a",
+    colorBlack_light: "#333",
+    colorBlack_medium: "#222",
+    colorBlack_dark: "#111",
+
+    colorPrimaryNormal: this.colorBlueNormal,
+    colorPrimaryActive: this.colorBlueActive,
+    colorSecondaryNormal: this.colorBlueNormal,
+    colorSecondaryActive: this.colorBlueActive,
+
+    color_text: this.colorBlack_dark,
+    color_bg: this.colorWhite,
+
+    color_linkHover: this.colorBlueActive,
+    color_linkNormal: this.colorBlueNormal,
+
+    font_familyPrimary: "Rubik",
+    font_familySecondary: "Roboto",
+
+    text_sizeLarge: 18,
+    text_sizeNormal: 15,
+    text_sizeSmall: 13,
+
+    borderRadiusMain: 5,
+
+    containerMax_width: 1170,
+
+    screenLaptop_width: 1024,
+    screenTablet_width: 768,
+    screenMobile_width: 425,
+}
